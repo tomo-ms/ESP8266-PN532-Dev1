329aa4e71ca8efedefb8af419e1649d3fc5eab91
tomo <masushio@gmail.com>
2016-09-12 22:48:20 +0900

    Code refactoring

--- src/FeliCa_card_detection.cpp
+++ src/FeliCa_card_detection.cpp
@@ -22,8 +22,6 @@
 #include <aJson.h>
 #include "password.h"
 
-#define DEBUG_ESP_WIFI
-
 // Msg LED: GPIO16(D0)   Error LED: GPIO4(D2)
 #define MSG_LED         16
 #define ERROR_LED       4
@@ -36,6 +34,7 @@ uint8_t        _prevIDm[8];
 unsigned long  _prevTime;
 
 ESP8266WiFiMulti WiFiMulti;
+long wifiTimeout = 60000;
 
 bool isError = false;
 
@@ -49,16 +48,18 @@ void blinkLED(uint8_t pin, uint16_t onTime, uint16_t offTime, uint8_t loop ) {
 }
 
 bool initPN532() {
+  Serial.println();
+  Serial.println("[PN532] Connecting PN532 board");
   nfc.begin();
   uint32_t versiondata = nfc.getFirmwareVersion();
   if (!versiondata)
   {
-    Serial.print("Didn't find PN53x board");
+    Serial.println("[PN532] Didn't find PN532 board");
     isError = true;
     return false;
   }
-  Serial.print("Found chip PN5"); Serial.println((versiondata >> 24) & 0xFF, HEX);
-  Serial.print("Firmware ver. "); Serial.print((versiondata >> 16) & 0xFF, DEC);
+  Serial.print("[PN532] Found chip PN5"); Serial.println((versiondata >> 24) & 0xFF, HEX);
+  Serial.print("[PN532] Firmware ver. "); Serial.print((versiondata >> 16) & 0xFF, DEC);
   Serial.print('.'); Serial.println((versiondata >> 8) & 0xFF, DEC);
 
   // Set PN532 param
@@ -70,94 +71,31 @@ bool initPN532() {
 
 bool connectWifi() {
   Serial.println();
-  Serial.println("Connecting to WiFi AP");
+  Serial.println("[WiFi] Connecting WiFi");
 
   // wait for WiFi connection
   WiFiMulti.addAP(MY_SSID1, MY_PASSWORD1);
   WiFiMulti.addAP(MY_SSID2, MY_PASSWORD2);
 
+  long start = millis();
   while (WiFiMulti.run() != WL_CONNECTED) {
+    if ( (millis() - start) >wifiTimeout ) {
+      Serial.println("[WiFi] WiFi connection failed");
+      isError = true;
+      return false;
+    }
     Serial.print(".");
     delay(1000);
   }
 
   Serial.println();
-  Serial.println("WiFi connected");
-  Serial.print("   IP address: "); Serial.println(WiFi.localIP());
+  Serial.println("[WiFi] WiFi connected");
+  Serial.print("[WiFi] IP address: "); Serial.println(WiFi.localIP());
   return true;
 }
 
-
-/*
-void requestURI2(uint8_t *idm) {
-  Serial.println("Sending card data to the server...");
-
-  aJsonObject *root, *msg;
-  root=aJson.createObject();
-  aJson.addStringToObject(root,"to", "/topics/news");
-  aJson.addItemToObject(root,"notification", msg = aJson.createObject());
-  char buf[40];
-  sprintf(buf, "Card: %02X%02X%02X%02X%02X%02X%02X%02X",
-    idm[0], idm[1], idm[2], idm[3], idm[4], idm[5], idm[6], idm[7]);
-  aJson.addStringToObject(msg, "text", buf);
-  aJson.addStringToObject(msg, "sound", "default");
-  char *payload=aJson.print(root);
-  char payloadLen[5];
-  sprintf(payloadLen, "%d", strlen(payload));
-
-  const char *host = "fcm.googleapis.com";
-  WiFiClient client;
-  if (!client.connect(host, 80)) {
-    Serial.println("Connection failed.");
-    return;
-  }
-  Serial.println("Connected to server.");
-
-  client.print("POST /fcm/send HTTP/1.1\n");
-  client.print("Host: fcm.googleapis.com\n");
-  client.print("Connection: close\n");
-  client.print("Authorization: ");
-  client.print(FIREBASE_KEY);
-  client.print("\nContent-Type: application/json\n");
-  client.print("Content-Length: ");
-  client.print(payloadLen);
-  client.print("\n\n");
-  client.print(payload);
-
-  // long interval = 4000;
-  // unsigned long currentMillis = millis(), previousMillis = millis();
-  // while(!client.available()){
-  //   if( (currentMillis - previousMillis) > interval ){
-  //     Serial.println("Timeout");
-  //     blinkLED(ERROR_LED, 300, 200, 3);
-  //     client.stop();
-  //     return;
-  //   }
-  //   currentMillis = millis();
-  // }
-
-  long interval = 4000;
-  unsigned long currentMillis = millis(), previousMillis = millis();
-
-  Serial.println("Server response:");
-  while (client.connected()) {
-    if ( client.available() ) {
-      char str=client.read();
-      Serial.print(str);
-    }
-    if( (currentMillis - previousMillis) > interval ){
-      Serial.println("Timeout");
-      blinkLED(ERROR_LED, 300, 200, 3);
-      client.stop();
-      return;
-    }
-    currentMillis = millis();
-  }
-  client.stop();
-}
-*/
-void requestURI2(uint8_t *idm) {
-  Serial.println("Sending card data to the server...");
+void sendNotification(uint8_t *idm) {
+  Serial.println("[HTTP] Sending card data to the server...");
 
   aJsonObject *root, *msg;
   root = aJson.createObject();
@@ -169,10 +107,7 @@ void requestURI2(uint8_t *idm) {
   aJson.addStringToObject(msg, "text", buf);
   aJson.addStringToObject(msg, "sound", "default");
   char *payload = aJson.print(root);
-  char payloadLen[5];
-  sprintf(payloadLen, "%d", strlen(payload));
 
-//  const char *host =       "fcm.googleapis.com";
   const char *url = "http://fcm.googleapis.com/fcm/send";
   HTTPClient http;
   http.begin(url);
@@ -182,9 +117,10 @@ void requestURI2(uint8_t *idm) {
   int httpCode = http.POST(payload);
 
   if (httpCode > 0) {
-    Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+    Serial.printf("[HTTP] POST response code: %d\n", httpCode);
     if (httpCode == HTTP_CODE_OK) {
       String payload = http.getString();
+      Serial.print("[HTTP] response body: ");
       Serial.println(payload);
     }
   }
@@ -199,15 +135,20 @@ void setup(void)
   analogWrite(MSG_LED, 255);
 
   Serial.begin(115200);
-  blinkLED(MSG_LED, 100, 100, 4); // wait Serial to be stabilized
-  Serial.println("Hello!");
+  Serial.println();
+  Serial.println();
+  Serial.println();
+  for (uint8_t t = 4; t > 0; t--) {
+    Serial.printf("[SETUP] WAIT %d...\n", t);
+    Serial.flush();
+    blinkLED(MSG_LED, 100, 100, 5); // wait Serial to be stabilized
+  }
 
   if ( !initPN532() )
     return;
   if ( !connectWifi() )
     return;
 }
-
 
 void loop(void)
 {
@@ -227,7 +168,7 @@ void loop(void)
 
   // Wait for an FeliCa type cards.
   // When one is found, some basic information such as IDm, PMm, and System Code are retrieved.
-  Serial.print("Waiting for an FeliCa card...  ");
+  Serial.print("[PN532] Waiting for an FeliCa card...  ");
   analogWrite(MSG_LED, 32);
   ret = nfc.felica_Polling(systemCode, requestCode, idm, pmm, &systemCodeResponse, 5000);
 
@@ -239,7 +180,8 @@ void loop(void)
   }
   if (ret != 1)
   {
-    Serial.print("Card access error: code "); Serial.println(ret);
+    Serial.println();
+    Serial.print("[PN532] Card access error: code "); Serial.println(ret);
     analogWrite(MSG_LED, 0);
     blinkLED(ERROR_LED, 100, 50, 3);
     delay(500);
@@ -258,18 +200,18 @@ void loop(void)
   }
   blinkLED(MSG_LED, 100, 50, 2);
   Serial.println("Found a card!");
-  Serial.print("  IDm: ");
+  Serial.print("[PN532]  IDm: ");
   nfc.PrintHex(idm, 8);
-  Serial.print("  PMm: ");
+  Serial.print("[PN532]  PMm: ");
   nfc.PrintHex(pmm, 8);
-  Serial.print("  System Code: ");
+  Serial.print("[PN532]  System Code: ");
   Serial.print(systemCodeResponse, HEX);
-  Serial.print("\n");
+  Serial.println();
 
   memcpy(_prevIDm, idm, 8);
   _prevTime = millis();
 
-  requestURI2(idm);
+  sendNotification(idm);
 
   // Wait 1 second before continuing
   Serial.println("Card access completed!\n");
